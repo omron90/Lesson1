3b9e5e84f86c584f0742b976645fe3a2cb064d62
vladislav_levin <vladislav.levin@mail.ru>
2025-03-07 14:27:30 +0400

    Lesson1-2

--- MainClassTest.java
+++ MainClassTest.java
@@ -1,15 +1,12 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainClassTest{
     @Test
-    public void getLocalNumber() {
-        //Создаём экземпляр класса MainClass
+    public void testGetClassNumber(){
         MainClass mainClass = new MainClass();
-        //Вызываем метод getLocalNumber и сохраняем результат
-        int result = mainClass.getLocalNumber();
-        //Проверяем, что результат равен 14
-        assertEquals(14, result, "Метод getLocalNumber должен возвращать число 14");
+        int actualResult = mainClass.getClassNumber();
+        assertTrue(actualResult > 45, "Ожидалось, что значение будет больше 45, но получено: " + actualResult);
     }
 }
